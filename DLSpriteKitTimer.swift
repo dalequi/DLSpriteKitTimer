a6a29d5f8253b0413e789179c68027577ab43a59
David Lechón Quiñones <dalequi@gmail.com>
2015-10-26 22:39:02 -0400

    First version of DLSpriteKitTimer

---
+++ DLSpriteKitTimer.swift
@@ -0,0 +1,114 @@
+//
+//  DLSpriteKitTimer.swift
+//  DLSpriteKitTimerDemo
+//
+//  Created by David Lechón Quiñones on 26/10/15.
+//  Copyright © 2015 dlqapps. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class DLSpriteKitTimer {
+    init(usingLabel label: SKLabelNode, seconds: Int, withActionPersecond: (() -> ())?, withFiringAction: () -> (), decreasing: Bool) {
+        self.running = false
+        self.label = label
+        self.actionPerSecond = withActionPersecond
+        self.firingAction = withFiringAction
+        self.decreasingTime = decreasing
+        if self.decreasingTime {
+            self.firstSecond = seconds
+            self.lastSecond = 0
+        } else {
+            self.firstSecond = 0
+            self.lastSecond = seconds
+        }
+        
+        self.counter = self.firstSecond
+        self.label.text = String(self.counter)
+    }
+    
+    func start() {
+        running = true
+        timerAction = SKAction.repeatActionForever(SKAction.sequence([actionwait,actionForEverySecond(actionPerSecond)]))
+        
+        if let timerAction = timerAction {
+            label.runAction(timerAction, withKey: "timer")
+        }
+    }
+    
+    func pause() {
+        running = false
+        label.removeActionForKey("timer")
+    }
+    
+    func stop() {
+        pause()
+        counter = firstSecond
+    }
+    
+    func reset() {
+        stop()
+        start()
+    }
+
+    
+    var label: SKLabelNode
+    
+    var running: Bool
+    
+    private let firstSecond: Int
+    
+    private let lastSecond: Int
+    
+    private let actionPerSecond: (() -> ())?
+    
+    private let firingAction: () -> ()
+    
+    private var decreasingTime = false
+
+    private var counter: Int {
+        didSet {
+            label.text = String(counter)
+        }
+    }
+    
+    private var timerAction: SKAction?
+    
+    private let actionwait = SKAction.waitForDuration(1)
+    
+    private func decreaseOrIncreaseTime() {
+        if decreasingTime {
+            if counter > lastSecond {
+                counter--
+            }
+            
+        } else {
+            if counter < lastSecond {
+                counter++
+            }
+        }
+        
+        if decreasingTime && counter == 0 || !decreasingTime && counter == lastSecond{
+            stop()
+            firingAction()
+        }
+    }
+    
+    private func actionForEverySecond(action: (() -> ())?) -> SKAction{
+        let actionRun = SKAction.runBlock({
+            self.decreaseOrIncreaseTime()
+            if let action = action {
+                if self.running {
+                    action()
+                }
+                
+            }
+            
+        })
+        
+        return actionRun
+    }
+    
+}
+
